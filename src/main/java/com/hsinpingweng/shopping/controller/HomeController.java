6bc504d3619cfd418cc910b37a0cb7ae196c569f
wenghsinping <ohmyhappy77@gmail.com>
2020-10-18 10:00:57 +0100

    add webconfig file

--- src/main/java/com/hsinpingweng/shopping/controller/HomeController.java
+++ src/main/java/com/hsinpingweng/shopping/controller/HomeController.java
@@ -1,15 +1,14 @@
-package com.hsinpingweng.shopping.controller;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
-public class HomeController {
-
-    @GetMapping("/")
-    public String home() {
-        return "home";
-    }
-
-
-}
+//package com.hsinpingweng.shopping.controller;
+//
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.GetMapping;
+//
+//@Controller
+//public class HomeController {
+//
+//    @GetMapping("/")
+//    public String home() {
+//        return "home";
+//    }
+//}
