f2996874f5740d2aef6ab7a6945f62036fcebd30
wenghsinping <ohmyhappy77@gmail.com>
2020-10-18 17:07:39 +0100

    add model pageRepository

--- src/main/java/com/hsinpingweng/shopping/controller/AdminPagesController.java
+++ src/main/java/com/hsinpingweng/shopping/controller/AdminPagesController.java
@@ -1,15 +1,33 @@
 package com.hsinpingweng.shopping.controller;
 
+import java.util.List;
+
+import com.hsinpingweng.shopping.model.PageRepository;
+import com.hsinpingweng.shopping.model.data.Page;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/admin/pages")
 public class AdminPagesController {
 
+    private PageRepository pageRepo;
+
+    public AdminPagesController(PageRepository pageRepo) {
+        this.pageRepo = pageRepo;
+    }
+
     @GetMapping
-    public String index() {
+    public String index(Model model) {
+
+        List<Page> pages = pageRepo.findAll();
+
+        model.addAttribute("pages", pages);
+        
         return "admin/pages/index";
     }
 }
---
+++ src/main/java/com/hsinpingweng/shopping/model/PageRepository.java
@@ -0,0 +1,11 @@
+package com.hsinpingweng.shopping.model;
+
+import com.hsinpingweng.shopping.model.data.Page;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface PageRepository extends JpaRepository<Page, Integer> {
+    
+
+}
