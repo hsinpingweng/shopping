d76209d7702f3357b74dae1d741123636a142cc8
wenghsinping <ohmyhappy77@gmail.com>
2020-10-19 11:12:05 +0100

    add category entity and repository

---
+++ src/main/java/com/hsinpingweng/shopping/model/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.hsinpingweng.shopping.model;
+
+import com.hsinpingweng.shopping.model.data.Category;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+    
+}
