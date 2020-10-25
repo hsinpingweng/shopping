dc9616e077bcbb327e5a215c82fbeba0d8e0d609
wenghsinping <ohmyhappy77@gmail.com>
2020-10-25 12:37:06 +0000

    add add/post func ro admin.category

--- src/main/java/com/hsinpingweng/shopping/controller/AdminCategoriesController.java
+++ src/main/java/com/hsinpingweng/shopping/controller/AdminCategoriesController.java
@@ -2,14 +2,19 @@ package com.hsinpingweng.shopping.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import com.hsinpingweng.shopping.model.CategoryRepository;
 import com.hsinpingweng.shopping.model.data.Category;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/admin/categories")
@@ -26,5 +31,43 @@ public class AdminCategoriesController {
         model.addAttribute("categories", categories);
 
         return "admin/categories/index";
+    }
+
+    // @GetMapping("/add")
+    // public String add(Category category) {
+    //     return "admin/categories/add";
+    // }
+
+    
+
+    @GetMapping("/add")
+    public String add(Category category) {
+        return "admin/categories/add";
+    }
+
+
+    @PostMapping("/add")
+    public String add(@Valid Category category, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        if(bindingResult.hasErrors()) return "admin/categories/add";
+
+        redirectAttributes.addFlashAttribute("message", "Category added");
+        redirectAttributes.addFlashAttribute("alertClass", "alert-success");
+
+        String slug = category.getName().toLowerCase().replace(" ", "-");
+    
+        Category categoryExist = categoryRepo.findByName(category.getName());
+        if (categoryExist != null) {
+            // add value in session
+            redirectAttributes.addFlashAttribute("message", "Category exists!");
+            redirectAttributes.addFlashAttribute("alertClass", "alert-danger");
+            redirectAttributes.addFlashAttribute("categoryInfo", category);
+        } else {
+            category.setSlug(slug);
+            category.setSorting(100);
+            categoryRepo.save(category);
+        }
+
+        return "redirect:/admin/categories/add";
     }
 }
