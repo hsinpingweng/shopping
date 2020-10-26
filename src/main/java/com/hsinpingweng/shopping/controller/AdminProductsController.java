279d4cd6fc805c4c561dc64611ba4b057877f035
wenghsinping <ohmyhappy77@gmail.com>
2020-10-26 21:47:51 +0000

    add product entity validation

--- src/main/java/com/hsinpingweng/shopping/controller/AdminProductsController.java
+++ src/main/java/com/hsinpingweng/shopping/controller/AdminProductsController.java
@@ -2,7 +2,9 @@ package com.hsinpingweng.shopping.controller;
 
 import java.util.List;
 
+import com.hsinpingweng.shopping.model.CategoryRepository;
 import com.hsinpingweng.shopping.model.ProductRepository;
+import com.hsinpingweng.shopping.model.data.Category;
 import com.hsinpingweng.shopping.model.data.Product;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,16 +18,30 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminProductsController {
 
     @Autowired
-    private ProductRepository productRepository;
+    private ProductRepository productRepo;
+
+    @Autowired
+    private CategoryRepository categoryRepo;
 
     @GetMapping
     public String index(Model model) {
 
-        List<Product> products = productRepository.findAll();
+        List<Product> products = productRepo.findAll();
 
         model.addAttribute("products", products);
 
         return "admin/products/index";
+    }
+
+    @GetMapping("/add")
+    public String add(Product product, Model model) {
+
+        List<Category> categories = categoryRepo.findAll();
+        model.addAttribute("categories", categories);
+
+        
+
+        return "admin/products/add";
     }
     
 }
