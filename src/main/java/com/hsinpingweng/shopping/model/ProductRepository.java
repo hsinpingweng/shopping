3370c0d48238846ff9541bd8aa48cbf6b49a8b79
wenghsinping <ohmyhappy77@gmail.com>
2020-11-15 17:39:50 +0000

    add ck editor and admin/product delete

--- src/main/java/com/hsinpingweng/shopping/model/ProductRepository.java
+++ src/main/java/com/hsinpingweng/shopping/model/ProductRepository.java
@@ -2,6 +2,8 @@ package com.hsinpingweng.shopping.model;
 
 import com.hsinpingweng.shopping.model.data.Product;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Integer> {
@@ -9,6 +11,8 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
 	Product findBySlug(String slug);
 
 	Product findBySlugAndIdNot(String slug, int id);
+
+	Page<Product> findAll(Pageable pageable);
 
 
 }
